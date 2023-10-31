aab0ed78bf168941632919472c70c5432e0293f2
Francis Waihiga <francisgwaihiga@gmail.com>
2023-10-31 22:10:29 +0000

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-# ifndef BINARY_TREES_H
-# define BINARY_TREES_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 /**
 * struct binary_tree_s - Binary tree node
 *
@@ -19,5 +19,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
 
-# endif
+#endif
