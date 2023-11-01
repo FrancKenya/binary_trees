16aa6da52f353c06784d2db1806b1dda38ba3d1b
status <Francis Waihiga>
2023-11-01 23:40:55 +0000

    task 12 - description

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,12 @@
+# include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - A function that counts the leaves in a BT
+ * @tree: count to the root node of the bt
+ *
+ * Return: Number of leaves or 0 if tree is NULL
+ * A NULL pointer is not a leaf
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
