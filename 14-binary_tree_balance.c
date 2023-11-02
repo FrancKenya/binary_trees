4481c284b6e145616225f1d4a8226a1096418684
FrancKenya <francisgwaihiga@gmail.com>
2023-11-02 17:21:16 +0300

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,13 +33,40 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	return (count_left - count_right);
 }
+
 /**
- * binary_tree_height - calculates the height of a tree
+ * binary_tree_height - A function that measures height of a binary tree
+ * @tree: points to the root of the tree
+ * Return: height of the tree or 0 if tree is NULL
  */
-int binary_tree_height(const binary_tree_t *tree)
+
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
+        size_t right_height, left_height, i;
 
-	}
+        if (!tree)
+        {
+                return (0);
+        }
+        /* find right height of the tree */
+        if (tree->right)
+        {
+                right_height = 1 + binary_tree_height(tree->right);
+        }
+        else
+        {
+                right_height = 0;
+        }
+        /* find left height of the tree */
+        if (tree->left)
+        {
+                left_height = 1 + binary_tree_height(tree->left);
+        }
+        else
+        {
+                left_height = 0;
+        }
 
+        i = (right_height > left_height ? right_height : left_height);
+        return (i);
+}
